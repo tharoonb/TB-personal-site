f1fa50aef6008eb3aeee97beae371e066ce0d98d
tharoonb <89992077+tharoonb@users.noreply.github.com>
2022-06-28 15:23:21 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">Tharoon Balaji
+      </h1>
+      <h2>Socials:
+        <img src="" onClick="" ></img>
+      </h2>
+      <body>  
+        <div className ="about">
+          A little about me:
+          <div className="blurb">
+            My name is Tharoon Balaji, a student currently studying Finance and Information Systems, with a minor in Spanish. I am a student Honors Finance Program as well, while also being involved in the Delta Sigma Pi
+            Nu Professional Business Fraternity serving as both an active brother and the Director of Alumni Database. I 
+          </div>
+        </div>
+        <div className="edu">
+          Education: The Ohio State University
+        </div>
+
+        <div>
+          
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
